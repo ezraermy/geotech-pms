d118723be9a4e07141bbac2371a8f811260b7f18
Ezra <ezraermy@gmail.com>
2023-11-09 12:37:41 +0300

    Create user madel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :assignments
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+# Attributes
+validates :username, presence: true, uniqueness: true
+validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
