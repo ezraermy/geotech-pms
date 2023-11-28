b5fbd87eddfb9eae4a5aca34cac53a7ce1e850f8
Ezra <ezraermy@gmail.com>
2023-11-28 15:06:10 +0300

    Add devise to the user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   namespace :api do
-    resources :users do
       resources :assignments
     end
     resources :projects
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_114850) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_09_091831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,10 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_114850) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "project_id", null: false
-    t.bigint "user_id", null: false
-    t.index ["project_id"], name: "index_assignments_on_project_id"
-    t.index ["user_id"], name: "index_assignments_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -43,6 +39,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_114850) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "assignments", "projects"
-  add_foreign_key "assignments", "users"
 end
