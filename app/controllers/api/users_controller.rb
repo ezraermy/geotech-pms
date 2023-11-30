23631bf2d4cc90bc77c977072da9a35d29b80904
Ezra <ezraermy@gmail.com>
2023-11-30 15:50:24 +0300

    Update user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,32 +1,27 @@
-class Api::UsersController < ApplicationController
-  def index
-    @user = User.find_by(user_name: params[:user_name])
-    if user
-      render json: user, status: :ok
-    else
-      render json: { message: 'User not found' }, status: :not_found
-    end
-  end
+# app/controllers/api/users_controller.rb
+module Api
+  class UsersController < ApplicationController
+    before_action :authenticate_user!, only: [:index]
 
-  def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+    def create
+      user = User.new(user_params)
 
-  def show
-    user = User.find_by(id: params[:id])
-    if user
-      render json: user, status: :ok
-    else
-      render json: { message: 'User not found' }, status: :not_found
+      if user.save
+        render json: { user: user }, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
     end
-  end
 
-  def user_params
-    params.require(:user).permit(:user_name)
+    def index
+      users = User.all
+      render json: { users: users }, status: :ok
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
   end
 end
